dc688dfb563de142b51ccfd6c57c954e573e0c6b
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-11-05 09:30:48 +0700

    Fix after logout

--- src/js/api/auth.js
+++ src/js/api/auth.js
@@ -16,17 +16,18 @@ export async function register({ email, password, username, avatar }) {
 	const { user } = await firebase
 		.auth()
 		.createUserWithEmailAndPassword(email, password)
-	await createUserProfile({
-		uid: user.uid,
-		username,
-		email,
-		avatar,
-		executedTests: [],
-	})
+	const userProfile = {uid: user.uid, username, email, avatar, executedTests: []}
+	await createUserProfile(userProfile)
+	return userProfile
 }
 
-export const login = ({ email, password }) =>
-	firebase.auth().signInWithEmailAndPassword(email, password)
+export const login = async ({ email, password }) => {
+	const { user } = await firebase
+		.auth()
+		.signInWithEmailAndPassword(email, password)
+	const userProfile = await getUserProfile(user.uid)
+	return userProfile
+}
 
 export const logout = () => firebase.auth().signOut()
 
